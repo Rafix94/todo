bc84bfffd807c557315009b312032b79e8adc998
Rafal Grzes <@noname>
2024-02-27 19:19:06 +0100

    added edge server

---
+++ edgeserver/src/main/java/com/recruitment/edgeserver/EdgeserverApplication.java
@@ -0,0 +1,30 @@
+package com.recruitment.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class EdgeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(predicateSpec -> predicateSpec
+                        .path("/todolist/useragent/**")
+                        .filters(filterSpec -> filterSpec.rewritePath("/todolist/useragent/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("userAgentCircuitBreaker")))
+                        .uri("lb://USERAGENT"))
+                .build();
+    }
+
+}
