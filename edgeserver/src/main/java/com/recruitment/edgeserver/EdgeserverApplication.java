78c8681806c4ec67e27d2d9ad94b54afda734549
System Administrator <root@air-aleksandra.home>
2024-03-09 15:13:09 +0100

    Extended circuite breaker timeout

--- edgeserver/src/main/java/com/recruitment/edgeserver/EdgeserverApplication.java
+++ edgeserver/src/main/java/com/recruitment/edgeserver/EdgeserverApplication.java
@@ -1,13 +1,17 @@
 package com.recruitment.edgeserver;
 
-import com.recruitment.edgeserver.filter.RequestLoggingFilter;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -18,16 +22,23 @@ public class EdgeserverApplication {
     }
 
     @Bean
-    public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder, RequestLoggingFilter requestLoggingFilter) {
+    public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(predicateSpec -> predicateSpec
                         .path("/todolist/useragent/**")
-                        .filters(filterSpec -> filterSpec.rewritePath("/todolist/useragent/(?<segment>.*)","/${segment}")
+                        .filters(filterSpec -> filterSpec.rewritePath("/todolist/useragent/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                                .circuitBreaker(config -> config.setName("userAgentCircuitBreaker"))
-                        )
-                        .uri("lb://USERAGENT"))
+                                .circuitBreaker(
+                                        circuitBreakerConfig -> circuitBreakerConfig.setName("userAgentCircuitBreaker")
+                                                .setFallbackUri("forward:/support"))
+                                ).uri("lb://USERAGENT"))
                 .build();
     }
 
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(20)).build()).build());
+    }
 }
