d280c4704fa5668c657effa6de61a63e4f23c133
Rafal <r.grzes94@gmail.com>
2024-11-10 14:20:37 +0100

    user agent cleanup

--- useragent/src/main/java/com/recruitment/useragent/dto/TeamSummaryDto.java
+++ useragent/src/main/java/com/recruitment/useragent/dto/TeamSummaryDto.java
@@ -2,12 +2,14 @@ package com.recruitment.useragent.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(
-        name = "MembershipDto",
-        description = "Object representing a team with membership information"
-)
+@Schema(name = "TeamSummaryDto", description = "Object representing a summary of a team with membership status")
 public record TeamSummaryDto(
+        @Schema(description = "The unique identifier of the team", example = "team-12345", required = true)
         String id,
+
+        @Schema(description = "The name of the team", example = "Engineering Team", required = true)
         String name,
+
+        @Schema(description = "Indicates whether the user is a member of the team", example = "true", required = true)
         boolean isMember
 ) {}
